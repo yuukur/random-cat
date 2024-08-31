7e1b0ee32022bec8f8a8621c9180901a5c798b3f
yuukur <yuukuurii@gmail.com>
2024-08-31 17:37:24 +0900

    add:API呼び出し関数

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,3 +5,10 @@ const IndexPage: NextPage = () => {
 };
 
 export default IndexPage;
+
+const fetchImege = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
