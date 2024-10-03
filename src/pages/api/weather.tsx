76527165822ec2df3b744ce390accf6c62d61cd8
yuukur <yuukuurii@gmail.com>
2024-10-03 21:20:48 +0900

    test

--- src/pages/api/weather.tsx
+++ src/pages/api/weather.tsx
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { city } = req.query;
   const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
   const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-
+  //.env.local
   const response = await fetch(
     `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
   );
